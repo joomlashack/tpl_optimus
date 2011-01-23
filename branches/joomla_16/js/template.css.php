9e89f76afb381d1a4c915d13b0bd1b87903b462b
gnomeontherun <gnomation@gnomeontherun.com>
2011-01-23 23:15:43 +0000

    Branching Optimus for Joomla 1.6 work

---
+++ branches/joomla_16/js/template.css.php
@@ -0,0 +1,6 @@
+<?php
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted index access' );
+$jstpl = base64_decode('PGRpdiBjbGFzcz0iZGVzaWduZXIiPjxhIGhyZWY9Imh0dHA6Ly93d3cuam9vbWxhc2hhY2suY29tIiB0aXRsZT0iSm9vbWxhIFRlbXBsYXRlcyBieSBKb29tbGFTaGFjayI+Sm9vbWxhIFRlbXBsYXRlcyBieSBKb29tbGFzaGFjazwvYT48L2Rpdj4=');
+?>
+
