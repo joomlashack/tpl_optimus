dd09e9b7064fc7c805b6c36df6b483bb9f9247b5
jmarkos <jkmarcosb@gmail.com>
2016-09-29 18:46:25 -0600

    adding style to blog

--- code/functions.php
+++ code/functions.php
@@ -9,3 +9,21 @@
 
 // Restrict Access to within Joomla
 defined('_JEXEC') or die('Restricted access');
+
+$app = JFactory::getApplication();
+$input = $app->input;
+
+$paramOption = $input->getVar('option', '');
+$paramView = $input->getVar('view', '');
+$paramLayout = $input->getVar('layout', 'default');
+$paramItemid = $input->getVar('Itemid', '');
+$paramId = $input->getVar('id', '');
+
+$isBlog = "not_blog";
+
+// Checks the right layout of the category, depending if it's set on the menu item or if it has to look for the category layout or default com_content layout for blogs
+if ($paramOption == 'com_content')
+{
+	if ($paramView == 'featured' || $paramView == 'category')
+	$isBlog = "is_blog";
+}
